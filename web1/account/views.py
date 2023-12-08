5d7c2d835b099f83e245ede2d200e25984d5ab54
kyounghoon96 <doctorkh5@naver.com>
2023-12-09 02:02:15 +0900

    메인화면, 로그인, 회원가입 페이지 대충 완성

--- web1/account/views.py
+++ web1/account/views.py
@@ -1,14 +1,34 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib import messages
 from django.contrib import auth
 
 # 회원가입
 def signup(request):
     if request.method == 'POST':
-        if request.POST['password'] == request.POST['confirm']:
-            user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        confirm_password = request.POST.get('confirm')
+
+        # 하나라도 안적은게 있을때
+        if not username or not password or not confirm_password:
+            messages.error(request, '모든 필드를 입력해주세요.')
+            return render(request, 'signup.html')
+        
+        # 이미 가입중인 user명
+        if User.objects.filter(username=username).exists():
+            messages.error(request, '이미 존재하는 사용자 이름입니다.')
+            return render(request, 'signup.html')
+
+        # 비밀번호, 비밀번호 확인 일치하는지
+        if password == confirm_password:
+            user = User.objects.create_user(username=username, password=password)
             auth.login(request, user)
             return redirect('/home/')
+        else:
+            messages.error(request, '비밀번호가 일치하지 않습니다.')
+            return render(request, 'signup.html')
+
     return render(request, 'signup.html')
 
 # 로그인
@@ -22,7 +42,12 @@ def login(request):
             auth.login(request, user)
             return redirect('/home/')
         else:
-            return render(request, 'login.html', {'error' : 'username or password is incorrect.'})
+            try:
+                user = User.objects.get(username=username)
+                messages.error(request, '아이디와 비밀번호가 일치하지 않습니다.')
+            except User.DoesNotExist:
+                messages.error(request, '존재하지 않는 아이디입니다.')
+            return render(request, 'login.html')
     else:
         return render(request, 'login.html')
 
