98cf34117769980bba4d7258ce8a046e9703389f
kyounghoon96 <kyounghoon155@gmail.com>
2023-12-13 01:28:03 +0900

    음식 분류 버튼 추가

--- web1/polls/views.py
+++ web1/polls/views.py
@@ -4,19 +4,22 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 import oracledb as od
 
+# db connection
 def connection():
     od.init_oracle_client(lib_dir=r"C:\Program Files\Oracle\instantclient_21_12")
     conn = od.connect(user='admin', password='INISW2inisw2', dsn='inisw2_high')
     return conn
 
+# /index
 def index(request):
     return render(request, 'polls/index.html')
 
-
+# /polls/test
 @csrf_exempt
 def test(request):
     return render(request, 'polls/test.html')
 
+# /polls/test-ajax
 @csrf_exempt
 def selecttest(request):
     # try:
@@ -26,7 +29,9 @@ def selecttest(request):
 
         # 검색 조건
         query = f'select nutrient, "에너지(Kcal)", "탄수화물", "단백질", "지방" from nutrient_data_table where nutrient like \'%{input_value}%\''
-        datas = connection(query)
+        exe = connection().cursor()
+        exe.execute(query)
+        datas = exe.fetchall()
 
         tests = []
         for data in datas:
@@ -47,15 +52,16 @@ def selecttest(request):
 
     return JsonResponse({'error': 'error'})
 
-
+# /home
 def home(request):
     return render(request, 'polls/home.html')
 
+# /input
 def input(request):
     return render(request, 'polls/input.html')
 
 
-# ajax test
+# /input - ajax test
 @csrf_exempt
 def input_ajax(request):
     if request.method == 'POST':
@@ -67,15 +73,32 @@ def input_ajax(request):
         }
     return JsonResponse(response_data)
 
-# form태그 test
+# /input - form태그 test
 def input_result(request):
     disease = request.POST.get('disease','')
     pill = request.POST.get('pill','')
     return render(request, 'polls/input_result.html', {'disease': disease, 'pill': pill})
 
+# /input - form태그 >> /recipe 페이지
+def input_recipe(request):
+    # 레시피 input페이지에서 recipe페이지로 보내기
+    recipe = request.POST.get('recipe','')
+    
+    # recipe페이지에서 버튼 생성할 value
+    query = f'select classification from tag_table group by CLASSIFICATION'
 
+    exe = connection().cursor()
+    exe.execute(query)
+    datas = exe.fetchall()
 
-# 자동완성 ajax
+    tests = []
+    for data in datas:
+        row = {'recipe': data}
+        tests.append(row)
+    
+    return render(request, 'polls/recipe.html', {'tests': tests, 'recipe': recipe})
+
+# /input - 자동완성 ajax
 @csrf_exempt
 def auto_complete(request):
     if request.method == 'POST':
@@ -84,7 +107,6 @@ def auto_complete(request):
         query = (
             "SELECT dname FROM med_test "
             "WHERE UPPER(dname) LIKE UPPER(:input_value)||'%' "
-            "ORDER BY CASE WHEN UPPER(dname) LIKE UPPER(:input_value)||'%' THEN 1 ELSE 2 END, dname"
                 )
         with conn.cursor() as cursor:
             cursor.execute(query, input_value=f'%{input_value}%')
@@ -92,10 +114,36 @@ def auto_complete(request):
 
         return JsonResponse(datas, safe=False)
 
-# 자동완성 후 클릭한 option ajax
+# /input - 자동완성 후 클릭한 option ajax
 @csrf_exempt
 def select_pill(request):
     if request.method == 'POST':
         selected_option = request.POST.get('selected_option', '')
         result = {'result': selected_option}
         return JsonResponse(result)
+    
+# 레시피와 버튼value를 가져와 쿼리문 실행
+@csrf_exempt
+def recipe_ajax(request):
+    if request.method == 'POST':
+        recvalue = request.POST.get('recvalue', '').strip() #공백제거
+        btnvalue = request.POST.get('btnvalue', '').strip()
+        query = f'''
+                  SELECT recipe_title, recipe_url
+                  FROM tag_table
+                  WHERE recipe_title LIKE \'%{recvalue}%\' AND CLASSIFICATION = \'{btnvalue}\'
+                  '''
+        exe = connection().cursor()
+        exe.execute(query)
+        datas = exe.fetchall()
+        
+        result = []
+        columns = [desc[0] for desc in exe.description]
+
+        for data in datas:
+            row = {columns[i]: data[i] for i in range(len(columns))}
+            result.append(row)
+
+        return JsonResponse({'results': result})
+
+    
